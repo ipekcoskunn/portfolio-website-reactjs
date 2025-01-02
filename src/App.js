667e12115822561c8fb7e9cdaf510374f760d481
ipekcoskun <ipekcoskun@omegabigdata.com>
2025-01-02 23:54:30 +0300

    tailwindcss downloaded and images added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <h1>My Portfolio</h1>
+    <div className="">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
