3450d36eb413d72f09fea08ed7e211c2ccb098ee
ipekcoskun <ipekcoskun@omegabigdata.com>
2025-01-10 23:25:28 +0300

    footer started

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="w-full py-20 h-auto border-b-[1px] border-b-black grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-4 gap-8">
+                <div className="w-full h-full flex flex-col gap-8">
+                    Footer
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
