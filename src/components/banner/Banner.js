2e0ac55e46189bad8c99f082769e0aebccf07eb4
ipekcoskun <ipekcoskun@omegabigdata.com>
2025-01-11 01:47:25 +0300

    some components added and react-scroll package imported

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import LeftBanner from './LeftBanner';
-import RightBanner from './RightBanner';
+// import LeftBanner from './LeftBanner';
+// import RightBanner from './RightBanner';
+
 const Banner = () => {
     return (
         <section
             id="home"
-            className="w-full pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
+            className="w-full h-[800px] pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
         >
-            <LeftBanner />
-            <RightBanner />
+            {/* <LeftBanner />
+            <RightBanner /> */}
         </section>
     );
 }
---
+++ src/components/features/Features.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+        <section id='features'>Features</section>
+    )
+}
+
+export default Features
